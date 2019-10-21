273b1096a8a08612c8f199fae7c1d1e80f7c3929
root <pradeepkumartammali@gmail.com>
2019-10-21 09:32:54 +0000

    Adding Approach-1 changes

---
+++ Approach-1/config.py
@@ -0,0 +1,32 @@
+# base URL for the data to fetch
+base_url = 'http://bynk-data-engineering-task.storage.googleapis.com/data/'
+# Customers data 
+customers_url = base_url + 'customers.json'
+# Customers visits data
+visits_url = base_url + 'visits.csv'
+# Customers loans data 
+loans_urls = ['loan-2017-10.csv',
+              'loan-2017-11.csv',
+              'loan-2017-12.csv',
+              'loan-2018-1.csv',
+              'loan-2018-10.csv',
+              'loan-2018-11.csv',
+              'loan-2018-12.csv',
+              'loan-2018-2.csv',
+              'loan-2018-3.csv',
+              'loan-2018-4.csv',
+              'loan-2018-5.csv',
+              'loan-2018-6.csv',
+              'loan-2018-7.csv',
+              'loan-2018-8.csv',
+              'loan-2018-9.csv',
+              'loan-2019-1.csv',
+              'loan-2019-10.csv',
+              'loan-2019-2.csv',
+              'loan-2019-3.csv',
+              'loan-2019-4.csv',
+              'loan-2019-5.csv',
+              'loan-2019-6.csv',
+              'loan-2019-7.csv',
+              'loan-2019-8.csv',
+              'loan-2019-9.csv']
